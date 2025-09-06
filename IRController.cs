01b7a9452ffe82694623d297fc3d3e32062ab8cc
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-09-06 03:54:58 +0200

    Add project files.

---
+++ IRController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture.Frames;
+using Windows.Media.Capture;
+using Windows.Media.Playback;
+using Windows.Media.MediaProperties;
+using Windows.Media;
+using System.Diagnostics;
+
+namespace IRCameraView
+{
+    public enum IRFrameFilter
+    {
+        None,
+        Raw,
+        Illuminated
+    }
+
+    public class IRController
+    {
+        public MediaFrameReader MediaFrameReader { get; private set; }
+        public MediaPlayer MediaPlayer { get; private set; }
+        public MediaCapture MediaCapture { get; private set; }
+        public List<MediaFrameSourceGroup> Devices { get; private set; }
+        public MediaFrameSourceGroup Device { get; private set; }
+
+        public IRFrameFilter FrameFilter { get; set; }
+
+        public delegate void FrameReady(SoftwareBitmap bitmap);
+        public event FrameReady OnFrameReady;
+
+        private SoftwareBitmap _backBuffer;
+
+        public IRController()
+        {
+            FrameFilter = IRFrameFilter.None;
+            MediaCapture = new MediaCapture();
+            LoadCameras(MediaFrameSourceKind.Infrared);
+
+            if (Devices.Count == 0)
+                throw new Exception("No infrared cameras were found.");
+        }
+
+        private List<MediaFrameSourceGroup> LoadCameras(MediaFrameSourceKind allowedKind)
+        {
+            return LoadCameras([allowedKind]);
+        }
+
+        private List<MediaFrameSourceGroup> LoadCameras(List<MediaFrameSourceKind> allowedKinds)
+        {
+            Devices = new List<MediaFrameSourceGroup>();
+            var devices = MediaFrameSourceGroup.FindAllAsync().AsTask().Result;
+                
+            // Filter out the IR camera's
+            foreach (var device in devices)
+                foreach (var sourceInfo in device.SourceInfos)
+                    if (allowedKinds.Contains(sourceInfo.SourceKind))
+                        Devices.Add(device);
+
+            return Devices;
+        }
+
+        public void SelectDevice(MediaFrameSourceGroup sourceGroup)
+        {
+            // Clean up previous MediaFrameReader
+            if (MediaFrameReader != null)
+            {
+                MediaFrameReader.FrameArrived -= FrameArrived;
+                MediaFrameReader.StopAsync().AsTask().Wait();
+                MediaFrameReader.Dispose();
+                MediaFrameReader = null;
+            }
+
+            // Clean up previous MediaCapture
+            if (MediaCapture != null)
+            {
+                MediaCapture.Dispose();
+                MediaCapture = null;
+            }
+
+            Device = sourceGroup;
+
+            MediaCapture = new MediaCapture();
+
+            MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings
+            {
+                SourceGroup = Device,
+                SharingMode = MediaCaptureSharingMode.SharedReadOnly,
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+                MemoryPreference = MediaCaptureMemoryPreference.Cpu
+            };
+
+            MediaCapture.InitializeAsync(settings).AsTask().Wait();
+
+            //    MediaEncodingProfile mediaEncodingProfile = new MediaEncodingProfile();
+
+            //MediaEncodingProfile encodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Qvga);
+
+            //    Windows.UI.Xaml.Controls.CaptureElement
+            //var mfExtension = await mediaSink.InitializeAsync(encodingProfile.Audio, encodingProfile.Video);
+            //MediaCapture.StartPreviewToCustomSinkAsync(mediaEncodingProfile, mediaExtension);
+            //MediaCapture.StartPreviewAsync().AsTask().Wait();
+
+            //var infraredTorchControl = MediaCapture.VideoDeviceController.InfraredTorchControl;
+
+            //if (infraredTorchControl.IsSupported)
+            //{
+            //    //infraredTorchControl.SupportedModes
+            //}
+
+            var frameSources = MediaCapture.FrameSources;
+            var frameSource = frameSources.First().Value;
+
+            var preferredFormat = frameSource.SupportedFormats.First();
+
+            frameSource.SetFormatAsync(preferredFormat).AsTask().Wait();
+
+            MediaFrameReader = MediaCapture.CreateFrameReaderAsync(frameSource).AsTask().Result;
+            MediaFrameReader.AcquisitionMode = MediaFrameReaderAcquisitionMode.Realtime;
+
+            MediaFrameReader.FrameArrived += FrameArrived;
+
+            MediaFrameReader.StartAsync().AsTask().Wait();
+        }
+
+        public void SelectDeviceByIndex(int index)
+        {
+            if (index < 0 || index >= Devices.Count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Invalid device index.");
+
+            SelectDevice(Devices[index]);
+        }
+
+        public List<string> GetDeviceNames()
+        {
+            return Devices.Select(d => d.DisplayName).ToList();
+        }
+
+        private void FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+        {
+            using (var frameReference = sender.TryAcquireLatestFrame())
+            {
+                var videoMediaFrame = frameReference?.VideoMediaFrame;
+
+                var softwareBitmap = videoMediaFrame?.SoftwareBitmap;
+
+                if (softwareBitmap == null) return;
+                if (softwareBitmap.BitmapPixelFormat != BitmapPixelFormat.Bgra8 || softwareBitmap.BitmapAlphaMode != BitmapAlphaMode.Premultiplied)
+                    softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+
+                softwareBitmap = Interlocked.Exchange(ref _backBuffer, softwareBitmap);
+                softwareBitmap?.Dispose();
+
+                SoftwareBitmap latestBitmap;
+                while ((latestBitmap = Interlocked.Exchange(ref _backBuffer, null)) != null)
+                {
+                    var isIlluminated = videoMediaFrame.InfraredMediaFrame.IsIlluminated; // This filter gives a similar result to having the torch enabled or disabled even if we can't control the torch. (It halves framerate tho)
+                    if (FrameFilter == IRFrameFilter.None || (!isIlluminated && FrameFilter == IRFrameFilter.Raw) || (isIlluminated && FrameFilter == IRFrameFilter.Illuminated))
+                        OnFrameReady(latestBitmap);
+                    //latestBitmap.Dispose(); Needs to be done by the event handler.
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace IRCameraView
+{
+    /// <summary>
+    /// The window that displays the camera feed.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private IRController _irController;
+        //private bool _isRecording = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _irController = new IRController();
+
+            // Populate ComboBox with device names
+            DeviceComboBox.ItemsSource = _irController.GetDeviceNames();
+            if (DeviceComboBox.Items.Count > 0)
+                DeviceComboBox.SelectedIndex = 0; // Optionally select the first device by default
+
+            StartCapture();
+        }
+
+        private void StartCapture()
+        {
+            imageElement.Source = new SoftwareBitmapSource();
+
+            _irController.OnFrameReady += IrController_OnFrameArrived;
+        }
+
+        private void IrController_OnFrameArrived(SoftwareBitmap bitmap)
+        {
+            if (imageElement.DispatcherQueue != null) imageElement.DispatcherQueue.TryEnqueue(async () =>
+            {
+                try
+                {
+                    var imageSource = (SoftwareBitmapSource)imageElement.Source;
+                    await imageSource.SetBitmapAsync(bitmap);
+                    bitmap.Dispose(); // Important to dispose of.
+                }
+                catch { }
+            });
+        }
+
+        private void FrameFilter_SelectionChanged(object sender, Microsoft.UI.Xaml.Controls.SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox && _irController != null)
+            {
+                _irController.FrameFilter = (IRFrameFilter)comboBox.SelectedIndex;
+            }
+        }
+
+        private void DeviceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DeviceComboBox.SelectedIndex >= 0)
+            {
+                _irController.SelectDeviceByIndex(DeviceComboBox.SelectedIndex);
+                // Optionally, update UI or start preview, etc.
+            }
+        }
+
+        private async void TakePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            //_irController.\
+
+            var photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync("IRPhoto.jpg", CreationCollisionOption.GenerateUniqueName);
+
+            var encodingProperties = new ImageEncodingProperties
+            {
+                Subtype = "Y800"
+            };
+
+            using var stream = await photoFile.OpenAsync(FileAccessMode.ReadWrite);
+            await _irController.MediaCapture.CapturePhotoToStreamAsync(encodingProperties, stream);
+        }
+
+        static StorageFile videoFile;
+
+        private async void TakeVideo_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton toggleButton)
+            {
+                if (toggleButton.IsChecked ?? true)
+                {
+                    videoFile = await KnownFolders.VideosLibrary.CreateFileAsync("IRRecording.mp4", CreationCollisionOption.GenerateUniqueName);
+                    var encodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto);
+
+                    await _irController.MediaCapture.StartRecordToStorageFileAsync(encodingProfile, videoFile);
+                }
+                else
+                {
+                    await _irController.MediaCapture.StopRecordAsync();
+
+                    var successDialog = new ContentDialog()
+                    {
+                        Title = "Recording Saved",
+                        Content = "Video saved to: " + videoFile.Path,
+                        CloseButtonText = "OK"
+                    };
+                    await successDialog.ShowAsync();
+                }
+            }
+        }
+    }
+}
